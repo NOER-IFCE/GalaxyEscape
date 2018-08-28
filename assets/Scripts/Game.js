cb8836d64e05dd511bf21fce40a62d13f05e1224
thomasce10 <40862669+thomasce10@users.noreply.github.com>
2018-08-28 14:06:03 -0300

    Tela de Lançamento
    *Script Inicial e Propriedades

--- assets/Scripts/Game.js
+++ assets/Scripts/Game.js
@@ -12,30 +12,21 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
-        // foo: {
-        //     // ATTRIBUTES:
-        //     default: null,        // The default value will be used only when the component attaching
-        //                           // to a node for the first time
-        //     type: cc.SpriteFrame, // optional, default is typeof default
-        //     serializable: true,   // optional, default is true
-        // },
-        // bar: {
-        //     get () {
-        //         return this._bar;
-        //     },
-        //     set (value) {
-        //         this._bar = value;
-        //     }
-        // },
+        massaLabel: {
+            default: null,
+            type: cc.Label
+        },
+        velocidadeLabel: {
+            default: null,
+            type: cc.Label
+        }
     },
+    
 
-    // LIFE-CYCLE CALLBACKS:
+    onLoad: function () {
+        var self = this;
+        self.scheduleOnce(function(){
 
-    // onLoad () {},
-
-    start () {
-
+        }, 2);
     },
-
-    // update (dt) {},
 });
--- library/imports/2d/2dda31ca-5542-4d6b-962c-34ac8c6411ba.js
+++ library/imports/2d/2dda31ca-5542-4d6b-962c-34ac8c6411ba.js
@@ -18,31 +18,20 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
-        // foo: {
-        //     // ATTRIBUTES:
-        //     default: null,        // The default value will be used only when the component attaching
-        //                           // to a node for the first time
-        //     type: cc.SpriteFrame, // optional, default is typeof default
-        //     serializable: true,   // optional, default is true
-        // },
-        // bar: {
-        //     get () {
-        //         return this._bar;
-        //     },
-        //     set (value) {
-        //         this._bar = value;
-        //     }
-        // },
+        massaLabel: {
+            default: null,
+            type: cc.Label
+        },
+        velocidadeLabel: {
+            default: null,
+            type: cc.Label
+        }
     },
 
-    // LIFE-CYCLE CALLBACKS:
-
-    // onLoad () {},
-
-    start: function start() {}
-}
-
-// update (dt) {},
-);
+    onLoad: function onLoad() {
+        var self = this;
+        self.scheduleOnce(function () {}, 2);
+    }
+});
 
 cc._RF.pop();
